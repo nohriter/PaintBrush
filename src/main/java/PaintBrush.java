f8537098d9be7ccba59d9845696266fb8b4dedb1
nohriter <nohriter@gmail.com>
2021-11-24 14:24:51 +0900

    Frame 기본설정

--- src/main/java/PaintBrush.java
+++ src/main/java/PaintBrush.java
@@ -1,2 +1,25 @@
-public class PaintBrush {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class PaintBrush extends Frame {
+
+    public PaintBrush() {
+
+        basicSetting();
+    }
+
+    private void basicSetting() {
+        setSize(1024, 600);
+        setTitle("Paint Brush");
+        setLocationRelativeTo(null);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        setVisible(true);
+    }
+
 }
