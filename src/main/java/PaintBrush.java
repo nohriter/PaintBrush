324f1d4d0cb2ea3b61624bd10b8a25cb2784052c
nohriter <nohriter@gmail.com>
2021-11-24 15:18:30 +0900

    그림 도구 목록 추가

--- src/main/java/PaintBrush.java
+++ src/main/java/PaintBrush.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -5,6 +6,32 @@ import java.awt.event.WindowEvent;
 public class PaintBrush extends Frame {
 
     public PaintBrush() {
+        //JRadioButton 생성
+        JRadioButton line = new JRadioButton("선");
+        JRadioButton circle = new JRadioButton("원");
+        JRadioButton rect = new JRadioButton("사각형", true);
+        JRadioButton roundRect = new JRadioButton("둥근사각형");
+        JRadioButton pen = new JRadioButton("연필");
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(line);
+        group.add(circle);
+        group.add(rect);
+        group.add(roundRect);
+        group.add(pen);
+
+        String[] color = {"빨강", "초록", "파랑", "노랑", "보라"};
+        JComboBox<String> combo = new JComboBox<>(color);
+
+        JPanel southP = new JPanel();
+        southP.add(line);
+        southP.add(circle);
+        southP.add(rect);
+        southP.add(roundRect);
+        southP.add(pen);
+        southP.add(combo);
+
+        add(southP);
 
         basicSetting();
     }
--- src/main/java/PaintCanvas.java
+++ src/main/java/PaintCanvas.java
@@ -1,2 +1,8 @@
-public class PaintCanvas {
+import java.awt.*;
+
+public class PaintCanvas extends Canvas {
+
+    public PaintCanvas() {
+        setBackground(new Color(239, 255, 239));
+    }
 }
